1f7f4c103060933d147525e2a683f11c8e446692
m.pimonov <off_mi4ka@vk.com>
2023-04-15 14:44:03 +0700

    add

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+  return (
+    <div>
+      <h1>
+        Каталог автомобилей в Новосибирске
+      </h1>
+    </div>
+  )
+}
+
+export default App
